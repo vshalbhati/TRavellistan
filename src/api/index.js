dc0c45b8e3fdbda7d1553900fa19a8f42e51b317
vshalbhati <vishalbhati294@gmail.com>
2022-08-22 22:04:21 +0530

    first

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+
+
+export const getPlacesData=async(type, sw,ne)=>{
+    try{
+        const {data :{data}} =await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,{
+                params: {
+                  bl_latitude: sw.lat,
+                  tr_latitude: ne.lat,
+                  bl_longitude: sw.lng,
+                  tr_longitude: ne.lng,
+                },
+                headers: {
+                  'x-rapidapi-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+                  'x-rapidapi-Host': 'travel-advisor.p.rapidapi.com'
+                }
+              }
+            );
+        return data;
+    }
+    catch(error){
+        console.log(error)
+    }
+}
